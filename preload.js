10e7ef11a648e929e45dc9521cac69e1268f87a4
Katsu <203713036+Karumes@users.noreply.github.com>
2026-09-01 12:22:54 +1200

    1

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  closeApp: () => ipcRenderer.send("close-app"),
-  saveSettings: (data) => ipcRenderer.invoke("save-settings", data),
-  loadSettings: () => ipcRenderer.invoke("load-settings"),
-  openExternal: (url) => ipcRenderer.send("open-external", url)
-});
